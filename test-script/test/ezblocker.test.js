74c4d48df5c24a7ebe60abc262e7cb8a7a280afe
Adam Wisniewski <adamw@mudbugmedia.com>
2019-01-08 14:55:32 -0600

    Remove specific test scripts

--- test-script/test/ezblocker.test.js
+++
@@ -1,9 +0,0 @@
-var redirect = require('../redirect.test.js')
-
-describe('ezblocker.com', function() {
-
-  describe('ezblocker.com', function() {
-    it('should redirect to http://www.teleflex.com/en/usa/productAreas/anesthesia/productGroups/airway-management/products/endobronchialBlockers/products/ez-blocker/index.html', () => redirect.check('ezblocker.com', null, 'http://www.teleflex.com/en/usa/productAreas/anesthesia/productGroups/airway-management/products/endobronchialBlockers/products/ez-blocker/index.html', 301));
-  });
-
-});
