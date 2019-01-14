c0c4dc8da3d036809d3eefabdd66a5983a6ed466
Adam Wisniewski <adamw@mudbugmedia.com>
2019-01-14 14:27:30 -0600

    Add Generic Spec Test Geneator

---
+++ utility-scripts/test_generator.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+if [[ $1 == *".conf" ]]; then
+	confsToSpec=$1
+else
+	confsToSpec="$1/*.conf"
+fi
+
+for filename in $confsToSpec; do
+ sitename=$(basename "$filename" .vhost.conf)
+ echo "$sitename"
+ rewriteTarget=$(awk '/RewriteRule/ {print $3} ' $filename)
+ sourceDirectory=$(dirname "$filename")
+ specDirectory=${sourceDirectory/conf.d/spec}
+ touch $specDirectory/$sitename.test.js
+ cat > $specDirectory/$sitename.test.js << EOF
+	var redirect = require('../redirect.test.js')
+
+	describe('$sitename.vhost.conf', function() {
+
+	  describe('$sitename', function() {
+	    it('should redirect to $rewriteTarget',
+	        () => redirect.check(
+	            '$sitename',
+	            null ,
+	            '$rewriteTarget'));
+	  });
+
+	});
+
+EOF
+done
